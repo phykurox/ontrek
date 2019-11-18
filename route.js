5ed816f4eab36502e0f5cb828f406094b92e464d
noahho123 <noahho123@hotmail.com>
2019-11-18 14:23:16 +0800

    Update route.js

--- route.js
+++ route.js
@@ -1,23 +1,51 @@
 import Feedback from "./screens/feedback"
-import Fpexp from "./screens/fp_exp"
+import FPEXP from "./screens/FPEXP"
 import Homepage from "./screens/homepage"
+import Rewards from "./screens/rewards"
+import Map from "./screens/map"
+import Search from "./screens/searchAddress"
+
+
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from "react-navigation"
 var app = createStackNavigator({
+    FPEXP: {
+        screen: FPEXP,
+        navigationOptions: () => ({
+            header: null
+        })
 
+    },
     Homepage:{
         screen: Homepage,
         navigationOptions: () => ({
             header: null
         })
     },
-    Fpexp: {
-        screen: Fpexp,
+    
+    Rewards:{
+        screen: Rewards,
         navigationOptions: () => ({
             header: null
         })
-
     },
+
+    Map:{
+        screen: Map,
+        navigationOptions: () => ({
+            header: null
+        })
+    },
+
+    Search:{
+        screen: Search,
+        navigationOptions: () => ({
+            header: null
+        })
+    },
+
+  
+
     Feedback: {
         screen: Feedback,
         navigationOptions: () => ({
